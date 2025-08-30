aa55b3688f2c7ee61bf8e73826bf178f13a6432e
Maxim Kramarenko <maximkr@gmail.com>
2025-08-31 01:43:49 +0300

    tinymce8

--- src/main/webapp/html/tiny_mce/ts_init.js
+++ src/main/webapp/html/tiny_mce/ts_init.js
@@ -1,67 +1,108 @@
-tinyMCE.init({
-    custom_shortcuts: false,
+tinymce.init({
+    // Селектор для текстовых областей с классом mceEditor
+    selector: 'textarea.mceEditor',
+    
+    // Основные настройки
     content_css: contextPath + "/tinymce.css",
-    gecko_spellcheck: true,
     convert_urls: false,
-    mode: "textareas",
-    theme: "advanced",
-    editor_selector: "mceEditor",
-    plugins: "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+    
+    // Плагины (обновленный список для TinyMCE 8)
+    plugins: 'autolink lists table save link anchor image preview media searchreplace fullscreen insertdatetime nonbreaking charmap directionality visualchars code advlist visualblocks emoticons pagebreak autoresize quickbars wordcount codesample importcss autosave',
+    
+    // Панель инструментов (объединенная из старых theme_advanced_buttons)
+    toolbar: [
+        'undo redo | bold italic underline strikethrough | copy cut paste | searchreplace',
+        'alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent',
+        'link unlink anchor image | forecolor backcolor | fontfamily fontsize',
+        'table | charmap emoticons | insertdatetime | templates | preview fullscreen | code btnCode'
+    ],
+    
+    // Настройки меню
+    menubar: 'file edit view insert format tools table help',
+    
+    // Настройки высоты и изменения размера
+    min_height: 300,
+    resize: true,
+    
+    // Блоки и новые строки
     forced_root_block: false,
     force_br_newlines: false,
     force_p_newlines: false,
-    theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,cut,copy,pastetext,pasteword,|,undo,redo,|,search,replace,|,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,|,outdent,indent,|,link,unlink,anchor,image,code",
-    theme_advanced_buttons2: "forecolor,backcolor,|,fontselect,fontsizeselect,|,tablecontrols,|,charmap,iespell,|,print,fullscreen,|,template,btnCode,",
-    theme_advanced_buttons3: "",
-    theme_advanced_toolbar_location: "top",
-    theme_advanced_toolbar_align: "left",
-    theme_advanced_statusbar_location: "bottom",
-    theme_advanced_resizing: true,
-    setup: function(a) {
-        a.addButton("btnCode", {
-            title: "Code",
-            image: "cssimages/code.png",
-            onclick: function() {
-                a.focus();
-                var b = a.selection.getContent({format: "text"});
-                a.selection.setContent("[code=java]" + b + "[/code]")
+    
+    // Проверка орфографии
+    browser_spellcheck: true,
+    
+    // Настройки изображений
+    image_advtab: true,
+    
+    // Настройки ссылок
+    link_assume_external_targets: false,
+    
+    // Автосохранение 
+    autosave_ask_before_unload: true,
+    autosave_interval: '30s',
+    autosave_prefix: 'tinymce-autosave-{path}{query}-{id}',
+    autosave_restore_when_empty: false,
+    autosave_retention: '2m',
+    
+    // Настройка пользовательских кнопок и функций
+    setup: function(editor) {
+        // Добавляем кастомную кнопку Code
+        editor.ui.registry.addButton('btnCode', {
+            text: 'Code',
+            icon: 'sourcecode',
+            tooltip: 'Insert Java Code Block',
+            onAction: function() {
+                editor.focus();
+                var selectedText = editor.selection.getContent({format: 'text'});
+                editor.selection.setContent('[code=java]' + selectedText + '[/code]');
             }
-        })
-    },
-    skin: "o2k7",
-    skin_variant: "silver",
-    template_external_list_url: "js/template_list.js",
-    external_link_list_url: "js/link_list.js",
-    external_image_list_url: "js/image_list.js",
-    media_external_list_url: "js/media_list.js",
-    template_cdate_classes: "cdate creationdate",
-    template_mdate_classes: "mdate modifieddate",
-    template_selected_content_classes: "selcontent",
-    template_cdate_format: "%m/%d/%Y : %H:%M:%S",
-    template_mdate_format: "%m/%d/%Y : %H:%M:%S",
-    template_replace_values: {
-        username: "Jack Black",
-        staffid: "991234"
-    },
-    template_templates: [
-        {title: "--------"},
-        {title: "FullPathLink", src: "template_macros/FullPathLink.html", description: "It converts tasks number to a link and replaces tasks name to full link"},
-        {title: "SimpleNameLink", src: "template_macros/SimpleNameLink.html", description: "It converts tasks number to a link"},
-        {title: "--------"},
-        {title: "SubtaskFilter", src: "template_macros/SubtaskFilter.html", description: "It converts tasks number to a table which has list of subtasks by filter"},
-        {title: "--------"},
-        {title: "UDFViewMacro", src: "template_macros/UDFViewMacro.html", description: "It converts tasks number to a view of udf"},
-        {title: "--------"},
-        {title: "BurndownChart", src: "template_macros/BurndownChart.html", description: "It converts tasks number to a Burndown Chat"},
-        {title: "PersonalActivityChart", src: "template_macros/PersonalActivityChart.html", description: "It converts tasks number to a PersonalActivity Chart"},
-        {title: "TeamActivityChart", src: "template_macros/TeamActivityChart.html", description: "It converts tasks number to a TeamActivity Chart"},
-        {title: "TrendChart", src: "template_macros/TrendChart.html", description: "It converts tasks number to a Trend Chart"},
-        {title: "SpentTimeChart", src: "template_macros/SpentTimeChart.html", description: "It converts tasks number to a SpentTime Chart"},
-        {title: "WorkloadChart", src: "template_macros/WorkloadChart.html", description: "It converts tasks number to a Workload Chart"},
-        {title: "StateChart", src: "template_macros/StateChart.html", description: "It converts tasks number to a State Chart"},
-        {title: "ResolutionChart", src: "template_macros/ResolutionChart.html", description: "It converts tasks number to a Resolution Chart"},
-        {title: "SyntaxHighlighter", src: "template_macros/SyntaxHighlighter.html", description: "It highlights the code"}
-    ]
+        });
+
+        // Добавляем кнопку для шаблонов (замена template плагина)
+        editor.ui.registry.addMenuButton('templates', {
+            text: 'Templates',
+            icon: 'template',
+            tooltip: 'Insert Template',
+            fetch: function(callback) {
+                var items = [
+                    {type: 'menuitem', text: 'FullPathLink', onAction: function() { insertTemplate(editor, 'FullPathLink.html'); }},
+                    {type: 'menuitem', text: 'SimpleNameLink', onAction: function() { insertTemplate(editor, 'SimpleNameLink.html'); }},
+                    {type: 'separator'},
+                    {type: 'menuitem', text: 'SubtaskFilter', onAction: function() { insertTemplate(editor, 'SubtaskFilter.html'); }},
+                    {type: 'separator'},
+                    {type: 'menuitem', text: 'UDFViewMacro', onAction: function() { insertTemplate(editor, 'UDFViewMacro.html'); }},
+                    {type: 'separator'},
+                    {type: 'menuitem', text: 'BurndownChart', onAction: function() { insertTemplate(editor, 'BurndownChart.html'); }},
+                    {type: 'menuitem', text: 'PersonalActivityChart', onAction: function() { insertTemplate(editor, 'PersonalActivityChart.html'); }},
+                    {type: 'menuitem', text: 'TeamActivityChart', onAction: function() { insertTemplate(editor, 'TeamActivityChart.html'); }},
+                    {type: 'menuitem', text: 'TrendChart', onAction: function() { insertTemplate(editor, 'TrendChart.html'); }},
+                    {type: 'menuitem', text: 'SpentTimeChart', onAction: function() { insertTemplate(editor, 'SpentTimeChart.html'); }},
+                    {type: 'menuitem', text: 'WorkloadChart', onAction: function() { insertTemplate(editor, 'WorkloadChart.html'); }},
+                    {type: 'menuitem', text: 'StateChart', onAction: function() { insertTemplate(editor, 'StateChart.html'); }},
+                    {type: 'menuitem', text: 'ResolutionChart', onAction: function() { insertTemplate(editor, 'ResolutionChart.html'); }},
+                    {type: 'menuitem', text: 'SyntaxHighlighter', onAction: function() { insertTemplate(editor, 'SyntaxHighlighter.html'); }}
+                ];
+                callback(items);
+            }
+        });
+    }
 });
+
+// Функция для загрузки и вставки шаблонов
+function insertTemplate(editor, templateFile) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', contextPath + '/template_macros/' + templateFile, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                editor.insertContent(xhr.responseText);
+            } else {
+                console.error('Ошибка загрузки шаблона:', templateFile);
+            }
+        }
+    };
+    xhr.send();
+}
 
 var is_new = true;

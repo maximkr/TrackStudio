8e4d9a1da14622a384c00467a89a594f2303cccb
Maxim Kramarenko <maximkr@gmail.com>
2025-08-31 20:35:57 +0300

    rollback tinymce

--- src/main/webapp/html/tiny_mce/ts_init.js
+++ src/main/webapp/html/tiny_mce/ts_init.js
@@ -1,102 +1,186 @@
-tinymce.init({
-    // Селектор для текстовых областей с классом mceEditor
-    selector: 'textarea.mceEditor',
-    
-    // Основные настройки
-    content_css: contextPath + "/tinymce.css",
-    convert_urls: false,
-    
-    // Плагины (совместимые с TinyMCE 5)
-    plugins: 'autolink lists table save link anchor image preview media searchreplace fullscreen insertdatetime nonbreaking charmap directionality visualchars code visualblocks emoticons autoresize wordcount template paste',
-    
-    // Панель инструментов (упрощенная для TinyMCE 8)
-    toolbar: [
-        'undo redo | bold italic underline strikethrough | searchreplace',
-        'alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent',
-        'link unlink anchor image | forecolor backcolor | fontsize',
-        'table | charmap | customTemplates | preview fullscreen | code btnCode'
-    ],
-    
-    // Настройки меню
-    menubar: 'file edit view insert format tools table help',
-    
-    // Настройки высоты и изменения размера
-    min_height: 300,
-    resize: true,
-    
-    // Блоки и новые строки
-    forced_root_block: false,
-    
-    // Проверка орфографии
-    browser_spellcheck: true,
-    
-    // Настройки изображений
-    image_advtab: true,
-    
-    // Настройки ссылок
-    link_assume_external_targets: false,
-    
-    // Автосохранение (упрощенная конфигурация)
-    autosave_interval: '30s',
-    
-    // Настройка пользовательских кнопок и функций
-    setup: function(editor) {
-        // Добавляем кастомную кнопку Code для TinyMCE 5 (правильный API)
-        editor.ui.registry.addButton('btnCode', {
-            text: 'Code',
-            icon: 'sourcecode',
-            tooltip: 'Insert Java Code Block',
-            onAction: function() {
-                editor.focus();
-                var selectedText = editor.selection.getContent({format: 'text'});
-                editor.selection.setContent('[code=java]' + selectedText + '[/code]');
-            }
-        });
-
-        // Добавляем кнопку для шаблонов (замена template плагина)
-        editor.ui.registry.addMenuButton('customTemplates', {
-            text: 'Templates',
-            icon: 'new-document',
-            tooltip: 'Insert Template',
-            fetch: function(callback) {
-                var items = [
-                    {type: 'menuitem', text: 'FullPathLink', onAction: function() { insertTemplate(editor, 'FullPathLink.html'); }},
-                    {type: 'menuitem', text: 'SimpleNameLink', onAction: function() { insertTemplate(editor, 'SimpleNameLink.html'); }},
-                    {type: 'separator'},
-                    {type: 'menuitem', text: 'SubtaskFilter', onAction: function() { insertTemplate(editor, 'SubtaskFilter.html'); }},
-                    {type: 'separator'},
-                    {type: 'menuitem', text: 'UDFViewMacro', onAction: function() { insertTemplate(editor, 'UDFViewMacro.html'); }},
-                    {type: 'separator'},
-                    {type: 'menuitem', text: 'BurndownChart', onAction: function() { insertTemplate(editor, 'BurndownChart.html'); }},
-                    {type: 'menuitem', text: 'PersonalActivityChart', onAction: function() { insertTemplate(editor, 'PersonalActivityChart.html'); }},
-                    {type: 'menuitem', text: 'TeamActivityChart', onAction: function() { insertTemplate(editor, 'TeamActivityChart.html'); }},
-                    {type: 'menuitem', text: 'TrendChart', onAction: function() { insertTemplate(editor, 'TrendChart.html'); }},
-                    {type: 'menuitem', text: 'SpentTimeChart', onAction: function() { insertTemplate(editor, 'SpentTimeChart.html'); }},
-                    {type: 'menuitem', text: 'WorkloadChart', onAction: function() { insertTemplate(editor, 'WorkloadChart.html'); }},
-                    {type: 'menuitem', text: 'StateChart', onAction: function() { insertTemplate(editor, 'StateChart.html'); }},
-                    {type: 'menuitem', text: 'ResolutionChart', onAction: function() { insertTemplate(editor, 'ResolutionChart.html'); }},
-                    {type: 'menuitem', text: 'SyntaxHighlighter', onAction: function() { insertTemplate(editor, 'SyntaxHighlighter.html'); }}
-                ];
-                callback(items);
-            }
-        });
+function pasteHandler(e) {
+    var cbData;
+    if (e.clipboardData) {
+        cbData = e.clipboardData;
+    } else if (window.clipboardData) {
+        cbData = window.clipboardData;
     }
-});
 
-// Функция для загрузки и вставки шаблонов
-function insertTemplate(editor, templateFile) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', contextPath + '/template_macros/' + templateFile, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                editor.insertContent(xhr.responseText);
-            } else {
-                console.error('Ошибка загрузки шаблона:', templateFile);
+    if (e.msConvertURL) {
+        var fileList = cbData.files;
+        if (fileList.length > 0) {
+            for (var i = 0; i < fileList.length; i++) {
+                var blob = fileList[i];
+                console.log("Image blob: " + blob);
+                readPastedBlob(blob);
             }
         }
-    };
-    xhr.send();
+    }
+    if (cbData && cbData.items) {
+        if ((text = cbData.getData("text/plain"))) {
+            // Text pasting is already handled
+            return;
+        }
+        for (var i = 0; i < cbData.items.length; i++) {
+            if (cbData.items[i].type.indexOf('image') !== -1) {
+                var blob = cbData.items[i].getAsFile();
+                readPastedBlob(blob);
+            }
+        }
+    }
+
+    function readPastedBlob(blob) {
+        if (blob) {
+            reader = new FileReader();
+            reader.onload = function(evt) {
+                pasteImage(evt.target.result);
+            };
+            reader.readAsDataURL(blob);
+        }
+    }
+
+    function pasteImage(source) {
+        var image = "<img src='" + source + "' data-mce-selected='1'></img>";
+        window.tinyMCE.execCommand('mceInsertContent', false, image);
+    }
 }
 
-var is_new = true;
+tinymce.init({
+    browser_spellcheck : true,
+    convert_urls: false,
+    selector: '.mceEditor',
+    menubar: false,
+    statusbar: false,
+    height: 150,
+    // forced_root_block : 'div',
+    // paste_as_text: true,
+    paste_data_images: true,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code',
+        'textcolor template'
+    ],
+    toolbar: 'insertfile undo redo | styleselect fontselect fontsizeselect | forecolor backcolor | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | template',
+    setup : function(editor)
+    {
+        editor.on('paste', pasteHandler);
+
+        editor.on('init', function()
+        {
+            this.getDoc().body.style.fontSize = '14px';
+        });
+
+        editor.on('BeforeSetContent', function (e) {
+            console.log(e.content);
+            var node = e.content;
+            if (node.indexOf('<span class="fancytree-expander">') !== -1) {
+                var start = node.substring(0, node.indexOf('<span class="fancytree-expander">'));
+                node = node.substring(node.indexOf('<span class="fancytree-expander">'), node.lenght);
+                node = node.substring(node.indexOf('title="') + 7, node.lenght);
+                node = start + '#' + node.substring(0, node.indexOf(' ')) + node.substring(node.indexOf('</span>') + 7, node.lenght);
+            }
+            e.content = node;
+            console.log(e.content);
+        });
+
+        editor.addMenuItem('syntax_highlight', {
+            text: 'Add code',
+            context: 'tools',
+            onclick: function() {
+                editor.insertContent(
+                    '[code=java][/code]'
+                );
+            }
+        });
+
+        editor.addMenuItem('video', {
+            text: 'Add video',
+            context: 'tools',
+            onclick: function() {
+                editor.insertContent(
+                    'Video{task_number:attach_id}'
+                );
+            }
+        });
+    },
+    templates : [
+        {
+            title : "--------"
+        },
+        {
+            title : "FullPathLink",
+            url : "template_macros/FullPathLink.html",
+            description : "It converts tasks number to a link and replaces tasks name to full link"
+        },
+        {
+            title : "SimpleNameLink",
+            url : "template_macros/SimpleNameLink.html",
+            description : "It converts tasks number to a link"
+        },
+        {
+            title : "--------"
+        },
+        {
+            title : "SubtaskFilter",
+            url : "template_macros/SubtaskFilter.html",
+            description : "It converts tasks number to a table which has list of subtasks by filter"
+        },
+        {
+            title : "--------"
+        },
+        {
+            title : "UDFViewMacro",
+            url : "template_macros/UDFViewMacro.html",
+            description : "It converts tasks number to a view of udf"
+        },
+        {
+            title : "--------"
+        },
+        {
+            title : "BurndownChart",
+            url : "template_macros/BurndownChart.html",
+            description : "It converts tasks number to a Burndown Chat"
+        },
+        {
+            title : "PersonalActivityChart",
+            url : "template_macros/PersonalActivityChart.html",
+            description : "It converts tasks number to a PersonalActivity Chart"
+        },
+        {
+            title : "TeamActivityChart",
+            src : "template_macros/TeamActivityChart.html",
+            description : "It converts tasks number to a TeamActivity Chart"
+        },
+        {
+            title : "TrendChart",
+            url : "template_macros/TrendChart.html",
+            description : "It converts tasks number to a Trend Chart"
+        },
+        {
+            title : "SpentTimeChart",
+            url : "template_macros/SpentTimeChart.html",
+            description : "It converts tasks number to a SpentTime Chart"
+        },
+        {
+            title : "WorkloadChart",
+            url : "template_macros/WorkloadChart.html",
+            description : "It converts tasks number to a Workload Chart"
+        },
+        {
+            title : "StateChart",
+            url : "template_macros/StateChart.html",
+            description : "It converts tasks number to a State Chart"
+        },
+        {
+            title : "ResolutionChart",
+            url : "template_macros/ResolutionChart.html",
+            description : "It converts tasks number to a Resolution Chart"
+        },
+        {
+            title : "SyntaxHighlighter",
+            url : "template_macros/SyntaxHighlighter.html",
+            description : "It highlights the code"
+        }
+    ]
+});
